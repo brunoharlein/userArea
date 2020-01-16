85fae422f7d01102bb217ce16e90282cdb360f04
brunoharlein <bruno@harlein.com>
2020-01-16 12:32:04 +0100

    check pseudo and mail ok

--- model/userModel.py
+++ model/userModel.py
@@ -8,3 +8,24 @@ class userModel():
         # Create a instance of the connection class to acces the database
         # Créer une instance de la classe de connexion pour accéder à la base de données
         self.db = connection()
+
+    def check_pseudo(self, pseudo):
+        """Given a pseudo return the corresponding user from the database
+            Étant donné un pseudo retour, l'utilisateur correspondant de la base de données"""
+        self.db.initialize_connection()
+        self.db.cursor.execute("SELECT * FROM users WHERE pseudo = %s", (pseudo,))
+        # Use fetchone to avoid having a list as a result
+        # Utilisez fetchone pour éviter d'avoir une liste à la suite
+        result = self.db.cursor.fetchone()
+        self.db.close_connection()
+        return result
+
+    def check_email(self, email):
+        """Given an email, check if it already exists and return it
+            Étant donné un e-mail, vérifiez s'il existe déjà et retournez-le"""
+        self.db.initialize_connection()
+        self.db.cursor.execute("SELECT email FROM users WHERE email = %s", (email,))
+        result = self.db.cursor.fetchall()
+        self.db.close_connection()
+        return result
+
