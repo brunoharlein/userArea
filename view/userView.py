715e9a87964c215e4d6a906a1053ac5b27bd65df
brunoharlein <bruno@harlein.com>
2020-01-16 15:37:11 +0100

    add profil method

--- view/userView.py
+++ view/userView.py
@@ -43,3 +43,47 @@ class userView():
             print("Identifiants incorrects")
         time.sleep(3)
 
+    def profil(self, user):
+        """Display information about a given user from the database
+        Let the user delete or update his profile
+           Afficher des informations sur un utilisateur donné à partir de la base de données
+           Laisser l'utilisateur supprimer ou mettre à jour son profil"""
+
+        # Instanciate the model to operate queries on users table
+        # Instancier le modèle pour exécuter des requêtes sur la table des utilisateurs
+        model = userModel()
+        # Show user
+        print("Bonjour voici nos informations sur vous : ")
+        print(
+            "nom : {}\nprénom : {}\nage : {}\npseudo : {}\nemail : {}\n"
+            .format(user['name'], user['firstname'], user['age'], user['pseudo'], user['email'])
+        )
+        # Get action from user
+        # Obtenir une action de l'utilisateur
+        print("Actions possible s: supprimer son compte, m: modifier une information, tapez entrée pour revenir à l'accueil")
+        action = input(": ")
+        # Call the right method based on the action
+        # Appeler la bonne méthode en fonction de l'action
+        if action == "s":
+            model.delete_user(user)
+        elif action == "m":
+            # Ask which column to change and the new value to set
+            # Demandez quelle colonne modifier et quelle nouvelle valeur définir
+            column = input("Que voulez-vous modifier ? (name, firstname, pseudo, email, age, password) : ")
+            value = input("Nouvelle valeur : ")
+            # If the user changes the password with crypt it
+            # Si l'utilisateur change le mot de passe avec crypt il
+            if column == "password":
+                value = hashlib.sha256(bytes(value, encoding="ascii")).hexdigest()
+            # Update the dictionnary representing the user
+            # Mettre à jour le dictionnaire représentant l'utilisateur
+            user[column] = value
+            # Update the database
+            # Mettre à jour la base de données
+            model.update_user(user)
+
+
+
+
+
+
