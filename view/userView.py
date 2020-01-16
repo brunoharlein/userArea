2e9fdf6fe369cdf1880d46d4054342137a0373a8
brunoharlein <bruno@harlein.com>
2020-01-16 14:03:44 +0100

    add userView import class and construct

---
+++ view/userView.py
@@ -0,0 +1,13 @@
+import hashlib
+import os
+import time
+from getpass import getpass
+
+from model.userModel import userModel
+
+class userView():
+    """View or controller taking care of all the logic related to user in the app.
+    Allow to login, create and show a user. When showing a user, allow to update or delete the user"""
+
+    def __init__(self):
+        pass
